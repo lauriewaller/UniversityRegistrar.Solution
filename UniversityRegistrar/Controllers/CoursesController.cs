8351e75a821e7588d3d1f895ad6d10f163342b74
Laurie Waller <lauriewaller7@gmail.com>
2021-05-23 16:22:18 -0700

    add shared directory, Index and Create methods
    Co-authored-by: Jesse Callahan <jessetylercallahan@gmail.com>
    Co-authored-by: Karlson Drendel <kdrendel99@gmail.com>

---
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class CoursesController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public CoursesController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+
+        public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+
+
+
+  }
+}
